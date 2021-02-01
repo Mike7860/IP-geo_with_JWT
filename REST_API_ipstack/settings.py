7bb9bb4eb37786140919f8cdd015218d004292ad
Mike7860 <michal.78.60@gmail.com>
2021-02-01 02:53:28 +0100

    JWT autentication added

--- REST_API_ipstack/settings.py
+++ REST_API_ipstack/settings.py
@@ -63,8 +63,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
-    #     'rest_framework_simplejwt.authentication.JWTAuthentication',),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',),
         # 'DEFAULT_AUTHENTICATION_CLASSES': (
         #     'rest_framework.authentication.TokenAuthentication',
     #     'rest_framework.authentication.SessionAuthentication',),
