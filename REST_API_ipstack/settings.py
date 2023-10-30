6389f367a4e9146b1ecb9fed5a3ad19cf6689622
Mike7860 <60574486+Mike7860@users.noreply.github.com>
2023-10-30 16:55:06 +0100

    Update settings.py
    minor

--- REST_API_ipstack/settings.py
+++ REST_API_ipstack/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g=p#oy1xldu4iy7=@nqw%h7^b5#7gx+2d87l6cq5#ux1w*y=+2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mj-restful-api.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 IPSTACK_APP_KEY = config('IPSTACK_APP_KEY', default='')
 
